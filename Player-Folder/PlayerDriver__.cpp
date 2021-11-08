595e24903e0ce497cb245209086d2092ddc9bdc5
LJC <liamjcarlisle@gmail.com>
2021-11-08 16:13:19 -0500

    Update PlayerDriver__.cpp
    Added test for access to Player's Hand.

--- Player-Folder/PlayerDriver__.cpp
+++ Player-Folder/PlayerDriver__.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main(){
     Player player1("Name");
-    cout << "The player's name is: " << player1.name;
+    cout << "The player's name is: " << player1.name << endl;
+    cout << "The player's hand is: " << *(player1.hand) << endl;
     return 0;
 }
