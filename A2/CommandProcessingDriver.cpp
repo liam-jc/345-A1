3c13e629d67f11116861e56b537af7a618c569fc
LJC <liamjcarlisle@gmail.com>
2021-11-09 14:14:13 -0500

    Add files via upload

---
+++ A2/CommandProcessingDriver.cpp
@@ -0,0 +1,15 @@
+#include "GameEngine.h"
+#include "CommandProcessing.h"
+
+
+using namespace std;
+
+int main(){
+    CommandProcessor cp;
+    GameEngine GE;
+    cp.getCommand();
+
+
+
+    return 0;
+}
