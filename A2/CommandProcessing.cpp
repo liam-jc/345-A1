e108d4d2d6d2d5af6e767927f21e64e89dd80233
LJC <liamjcarlisle@gmail.com>
2021-11-09 13:48:51 -0500

    Update CommandProcessing.cpp

--- A2/CommandProcessing.cpp
+++ A2/CommandProcessing.cpp
@@ -1,2 +1,52 @@
 #include "CommandProcessing.h"
 
+
+
+
+
+
+
+
+
+
+//********CommandProcessor**********
+void CommandProcessor::saveCommand(string s){
+    Command c;
+    c.command = s; //assign command type from input
+    c.saveEffect(s);
+    this->commandCollection.push_back(c); //save the command to collection
+};
+
+void CommandProcessor::readCommand(){
+    cout << "Please input a command." << endl;
+    cin >> this->commandStr;
+    saveCommand(commandStr);
+};
+
+void CommandProcessor::getCommand(){
+    readCommand();
+}
+
+
+//********Command**********
+void Command::saveEffect(string s){
+    //assign effect corresponding to input
+    if (s == "loadmap *"){ //TODO: replace * with regex 
+        this->effect = "Load map from file, transition to maploaded state.";
+    }
+    if (s == "validatemap"){
+        this->effect = "Validate map, transition to mapvalidated state.";
+    }
+    if (s == "addplayer *"){ //TODO: replace * with regex 
+        this->effect = "Adds a player, transition to playersadded state.";
+    }
+    if (s == "gamestart"){
+        this->effect = "Start game, transition to assignreinforcement state.";
+    }
+    if (s == "replay"){
+        this->effect = "Restart new game, transition to start state.";
+    }
+    if (s == "quit"){
+        this->effect = "Quit game, exit program.";
+    }
+};
