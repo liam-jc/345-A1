b0b4ee988f19c7ea923eb0b1b56af67d7ce6169f
LJC <liamjcarlisle@gmail.com>
2021-11-10 16:50:19 -0500

    Update CommandProcessingDriver.cpp

--- A2/CommandProcessingDriver.cpp
+++ A2/CommandProcessingDriver.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main(){
     string userInput;
-    cout << "Welcome to Warzone CommandProcessor. \nWould you like to load commands from console or a file?" << endl;
+    cout << "Welcome to Warzone CommandProcessor. \nWould you like to load commands from console or a file? \n(Type '-console' or '-file')" << endl; //-file should be -file <filename>
     cin >> userInput;
-    if (userInput == "console"){    
-        //console option uses CommandProcessor
-        CommandProcessor cp; //make a CommandProcessor
+    if (userInput == "-console"){ //console option uses CommandProcessor      
+        
         //GameEngine GE;
+        CommandProcessor cp; //make a CommandProcessor
         cp.getCommand(); //request command input from terminal
         cp.getCommand(); //request another command input from terminal
 
@@ -19,8 +19,12 @@ int main(){
             cout << cp.commandCollection.at(i);
         }
     }
-    else if (userInput == "file"){
-        //file option uses FileCommandProcessorAdapter
+    else if (userInput == "-file"){ //file option uses FileCommandProcessorAdapter
+        FileCommandProcessorAdapter fcpa;
+        fcpa.getCommand(); //request command input from file
+        for (int i=0; i<fcpa.commandCollection.size(); i++){ //show commands
+            cout << fcpa.commandCollection.at(i);
+        }
     }
     else {
         cout << "Invalid input. Goodbye!";
