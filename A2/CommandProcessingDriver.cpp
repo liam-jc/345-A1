120c5f7e82d0704534e7307ec483917631a6f82a
LJC <liamjcarlisle@gmail.com>
2021-11-10 10:07:43 -0500

    Update CommandProcessingDriver.cpp

--- A2/CommandProcessingDriver.cpp
+++ A2/CommandProcessingDriver.cpp
@@ -1,14 +1,18 @@
-#include "GameEngine.h"
+// #include "GameEngine.h"
 #include "CommandProcessing.h"
 
 
 using namespace std;
 
 int main(){
-    CommandProcessor cp;
-    GameEngine GE;
-    cp.getCommand();
+    CommandProcessor cp; //make a CommandProcessor
+    //GameEngine GE;
+    cp.getCommand(); //request command input from terminal
+    cp.getCommand(); //request another command input from terminal
 
+    for (int i=0; i<cp.commandCollection.size(); i++){ //show commands
+        cout << cp.commandCollection.at(i);
+    }
 
 
     return 0;
