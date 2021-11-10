73ac5e7cbe3d029dd4baf365de43dd654f8f2c3b
LJC <liamjcarlisle@gmail.com>
2021-11-10 16:50:49 -0500

    Update CommandProcessing.h

--- A2/CommandProcessing.h
+++ A2/CommandProcessing.h
@@ -6,9 +6,11 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
+// class GameEngine;
 class Command;
 class CommandProcessor{
     private:      
@@ -22,11 +24,22 @@ class CommandProcessor{
     public:     
         vector<Command> commandCollection;
         string commandStr;
+        int currentState;
+           /*
+            * state 0 = Start();
+            *       1 = MapLoaded();
+            *       2 = MapValidated();
+            *       3 = PlayersAdded();
+            *       4 = AssignReinforcement();
+            *       5 = IssueOrders();
+            *       6 = ExecuteOrders();
+            *       7 = Win();
+            */
         CommandProcessor(); //default constructor
         CommandProcessor(const CommandProcessor &commandProcessorToCopy); //copy costructor
         friend ostream& operator << (ostream& outputStream, const CommandProcessor& c); //outstream overload
         void getCommand();
-        //void saveEffect(Command inputCommand);
+        // int getGameEngineState(GameEngine &ge1);
         bool validate(Command toValidate);
         ~CommandProcessor(); //destructor
 };
@@ -36,43 +49,63 @@ class Command{
     
 
     public:
-    void saveEffect(string s);
-    string command;
-    string effect;
-    Command(); //default constructor
-    Command(const Command &commandToCopy); //copy costructor
-    friend ostream& operator << (ostream& outputStream, const Command& c); //outstream overload
-    ~Command(); //destructor
+        void saveEffect(string s);
+        string command;
+        string effect;
+        Command(); //default constructor
+        Command(const Command &commandToCopy); //copy costructor
+        friend ostream& operator << (ostream& outputStream, const Command& c); //outstream overload
+        ~Command(); //destructor
 };
-
-// class FileLineReader;
-// class FileCommandProcessorAdapter:CommandProcessor{
-//     private:      
-//         // readCommand: 
-//         // •gets commands from the console as a string,
-//         // •stores the command internally in a collection of Command objects using saveCommand()
-//         // •provides a public getComman() to other objects such as the GameEngine or the Player
-//         void readCommand(); 
-
-//     public:     
-//         string commandStr;
-//         FileLineReader flr;
-//         FileCommandProcessorAdapter(); //default constructor
-//         FileCommandProcessorAdapter(const FileCommandProcessorAdapter &commandProcessorToCopy); //copy costructor
-//         friend ostream& operator << (ostream& outputStream, const FileCommandProcessorAdapter& c); //outstream overload
-//         void getCommand();
-//         ~FileCommandProcessorAdapter(); //destructor
-// };
 
 class FileLineReader{
     private:
-    void readLineFromFile();
+        string readLineFromFile();
 
     public:
-    FileLineReader(); //default constructor
-    FileLineReader(const FileLineReader &commandToCopy); //copy costructor
-    friend ostream& operator << (ostream& outputStream, const FileLineReader& c); //outstream overload
-    ~FileLineReader(); //destructor
+        friend class FileCommandProcessorAdapter; //to allow access from FileCommandProcessorAdapter
+        // fstream newfile;
+        FileLineReader(); //default constructor
+        FileLineReader(int, FileLineReader*);
+        FileLineReader(const FileLineReader &input); //copy costructor
+        friend ostream& operator << (ostream& outputStream, const FileLineReader& c); //outstream overload
+        ~FileLineReader(); //destructor
 };
+
+
+// class FileLineReader;
+class FileCommandProcessorAdapter: public CommandProcessor{
+    private:      
+        // readCommand: 
+        // •gets commands from a file,
+        // •stores the command internally in a collection of Command objects using saveCommand()
+        // •provides a public getCommand() to other objects such as the GameEngine or the Player
+        void readCommand(); 
+        void saveCommand(string s);
+
+    public:     
+        vector<Command> commandCollection;
+        string commandStr;
+        int currentState;
+           /*
+            * state 0 = Start();
+            *       1 = MapLoaded();
+            *       2 = MapValidated();
+            *       3 = PlayersAdded();
+            *       4 = AssignReinforcement();
+            *       5 = IssueOrders();
+            *       6 = ExecuteOrders();
+            *       7 = Win();
+            */
+        // FileLineReader* flr = new FileLineReader();
+        FileLineReader flr;
+        FileCommandProcessorAdapter(); //default constructor
+        FileCommandProcessorAdapter(const FileCommandProcessorAdapter &commandProcessorToCopy); //copy costructor
+        friend ostream& operator << (ostream& outputStream, const FileCommandProcessorAdapter& c); //outstream overload
+        void getCommand();
+        ~FileCommandProcessorAdapter(); //destructor
+};
+
+
 
 #endif
