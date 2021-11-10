2d2a01941d17c76e29e1634f39c93c350bd3e83f
LJC <liamjcarlisle@gmail.com>
2021-11-10 10:17:10 -0500

    Update CommandProcessingDriver.cpp

--- A2/CommandProcessingDriver.cpp
+++ A2/CommandProcessingDriver.cpp
@@ -5,15 +5,26 @@
 using namespace std;
 
 int main(){
-    CommandProcessor cp; //make a CommandProcessor
-    //GameEngine GE;
-    cp.getCommand(); //request command input from terminal
-    cp.getCommand(); //request another command input from terminal
+    string userInput;
+    cout << "Welcome to Warzone CommandProcessor. \nWould you like to load commands from console or a file?" << endl;
+    cin >> userInput;
+    if (userInput == "console"){    
+        //console option uses CommandProcessor
+        CommandProcessor cp; //make a CommandProcessor
+        //GameEngine GE;
+        cp.getCommand(); //request command input from terminal
+        cp.getCommand(); //request another command input from terminal
 
-    for (int i=0; i<cp.commandCollection.size(); i++){ //show commands
-        cout << cp.commandCollection.at(i);
+        for (int i=0; i<cp.commandCollection.size(); i++){ //show commands
+            cout << cp.commandCollection.at(i);
+        }
     }
-
+    else if (userInput == "file"){
+        //file option uses FileCommandProcessorAdapter
+    }
+    else {
+        cout << "Invalid input. Goodbye!";
+    }
 
     return 0;
 }
